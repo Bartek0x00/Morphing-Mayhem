bd4517970c1977576211ecbdcf7a8f8995fa441e
Bartek0x00 <rosbar008@gmail.com>
2024-07-12 18:11:39 +0200

    slight fixes

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -2,14 +2,14 @@ extends Node2D
 
 func _ready():
 	toggle_pause()
-	var color_rect = ColorRect.new()
+	var color_rect := ColorRect.new()
 	color_rect.color = Color(0, 0, 0, 0.73)
 	color_rect.size = Vector2(640, 128)
 	color_rect.position = Vector2(0, 480)
 	
-	var ls = LabelSettings.new()
+	var ls := LabelSettings.new()
 	ls.font_size = 64
-	var label = Label.new()
+	var label := Label.new()
 	label.text = "Level" + str(Score.level)
 	label.size = Vector2(200, 88)
 	label.position = Vector2(220, 16)
@@ -17,7 +17,7 @@ func _ready():
 	
 	color_rect.add_child(label)
 	add_child(color_rect)
-	var tween = get_tree().create_tween()
+	var tween := get_tree().create_tween()
 	tween.tween_property(color_rect, "modulate:a", 0, 3)
 	tween.tween_callback(color_rect.queue_free)
 	tween.tween_callback(toggle_pause)
